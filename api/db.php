2588820f8929ce1823edd36e8206d8b15bf13028
dean5201314 <smile90624@gmail.com>
2023-12-20 16:15:08 +0800

    class DB ____construct

--- api/db.php
+++ api/db.php
@@ -3,6 +3,19 @@ date_default_timezone_set("Asia/Taipei");
 session_start();
 
 class DB{
+    protected $dsn="mysql:localhost;charset:utf8;dbname=db10;";
+    protected $pdo;
+    protected $table;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn,'root','');        
+    }
+
+    public function all($where='',$other=''){
+        
+    }
     
 }
 
